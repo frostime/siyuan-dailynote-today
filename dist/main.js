f2f97a70f55f28ce97c1ed4569149323e876b77a
frostime <frostime@foxmail.com>
2023-04-02 20:34:25 +0800

    不再跟踪 dist

--- dist/main.js
+++
@@ -1,1 +0,0 @@
-"use strict";const l=require("siyuan");class e extends l.Plugin{constructor(){super()}onload(){this.el=document.createElement("button"),this.el.innerText="Hello World",l.clientApi.addToolbarRight(this.el),console.log("plugin load")}onunload(){console.log("plugin unload")}}module.exports=e;
