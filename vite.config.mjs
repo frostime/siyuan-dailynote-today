2a04ed97e88f805aa64f39c3406989f2409f5982
frostime <frostime@foxmail.com>
2023-04-07 15:46:48 +0800

    配置加入svelte后的环境

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,8 +1,10 @@
 // vite.config.js
 import { resolve } from 'path'
 import { defineConfig } from 'vite'
+import { svelte } from '@sveltejs/vite-plugin-svelte'
 
 export default defineConfig({
+    plugins: [svelte()],
     build: {
         lib: {
             entry: resolve(__dirname, 'src', "index.ts"),
