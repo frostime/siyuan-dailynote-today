7535cd17b9f8849c637cdc84ecaff4662ea8db7f
frostime <frostime@foxmail.com>
2023-04-29 17:07:18 +0800

    添加两个 ts 文件作为组件类

---
+++ src/toolbar-menu.ts
@@ -0,0 +1,13 @@
+import { Menu, MenuItem } from "siyuan";
+import { moveBlocksToDailyNote } from "./func";
+import { settings } from "./setting";
+import { Notebook } from "./types"
+
+export class SelectMenu {
+    ele: HTMLElement;
+    constructor () {
+        this.ele = document.createElement('button');
+        this.ele.className = 'toolbar__item b3-tooltips b3-tooltips__sw';
+        this.ele.setAttribute('aria-label', 'Open Today\'s Diary');
+    }
+}
