8e3b197b0a040216b67f72b0b585f5b43ea745ee
frostime <frostime@foxmail.com>
2023-05-13 01:39:19 +0800

    更新前端API

--- src/siyuan.d.ts
+++ src/siyuan.d.ts
@@ -1,10 +1,10 @@
-declare module "siyuan"{
+declare module "siyuan" {
     type TEventBus = "ws-main"
 
     interface IObject {
         [key: string]: string;
     }
-    
+
     interface IWebSocketData {
         cmd: string
         callback?: string
@@ -13,7 +13,15 @@ declare module "siyuan"{
         code: number
         sid: string
     }
-    
+
+    interface IPluginDockTab {
+        position: "LeftTop" | "LeftBottom" | "RightTop" | "RightBottom" | "BottomLeft" | "BottomRight",
+        size: { width: number, height: number },
+        icon: string,
+        hotkey?: string,
+        title: string,
+    }
+
     interface IMenuItemOption {
         label?: string,
         click?: (element: HTMLElement) => void,
@@ -28,13 +36,13 @@ declare module "siyuan"{
         current?: boolean
         bind?: (element: HTMLElement) => void
     }
-    
+
     export function fetchPost(url: string, data?: any, cb?: (response: IWebSocketData) => void, headers?: IObject): void;
-    
+
     export function fetchSyncPost(url: string, data?: any): Promise<IWebSocketData>;
-    
+
     export function fetchGet(url: string, cb: (response: IWebSocketData) => void): void;
-    
+
     export function openTab(options: {
         custom?: {
             title: string,
@@ -47,38 +55,40 @@ declare module "siyuan"{
         removeCurrentTab?: boolean // 在当前页签打开时需移除原有页签
         afterOpen?: () => void // 打开后回调
     }): void
-    
+
     export function isMobile(): boolean;
-    
+
+    export function adaptHotkey(hotkey: string): string;
+
     export function confirm(title: string, text: string, confirmCB?: () => void, cancelCB?: () => void): void;
-    
+
     /**
      * @param timeout - ms. 0: manual close；-1: always show; 6000: default
      * @param {string} [type=info]
      */
     export function showMessage(text: string, timeout?: number, type?: "info" | "error", id?: string): void;
-    
+
     export class App {
         plugins: Plugin[];
     }
-    
+
     export abstract class Plugin {
         eventBus: EventBus;
         i18n: IObject;
         data: any;
         name: string;
-    
+
         constructor(options: {
             app: App,
             id: string,
             name: string,
             i18n: IObject
         })
-    
+
         onload(): void;
-    
+
         onunload(): void;
-    
+
         /*
          * @param {string} [options.position=right]
          */
@@ -88,40 +98,50 @@ declare module "siyuan"{
             callback: (evt: MouseEvent) => void
             position?: "right" | "left"
         }): HTMLDivElement;
-    
+
         openSetting(): void
-    
+
         // registerCommand(command: IPluginCommand): void;
-    
+
         // registerSettingRender(settingRender: SettingRender): void;
-    
+
         loadData(storageName: string): Promise<any>;
-    
+
         saveData(storageName: string, content: any): Promise<void>;
-    
-        createTab(options: {
+
+        addTab(options: {
             type: string,
             destroy?: () => void,
             resize?: () => void,
             update?: () => void,
             init: () => void
         }): () => any
+
+        addDock(options: {
+            config: IPluginDockTab,
+            data: any,
+            type: string,
+            destroy?: () => void,
+            resize?: () => void,
+            update?: () => void,
+            init: () => void
+        }): any
     }
-    
+
     export class EventBus {
         on(type: TEventBus, listener: (event: CustomEvent<any>) => void): void;
-    
+
         once(type: TEventBus, listener: (event: CustomEvent<any>) => void): void;
-    
+
         off(type: TEventBus, listener: (event: CustomEvent<any>) => void): void;
-    
+
         emit(type: TEventBus, detail?: any): boolean;
     }
-    
+
     export class Dialog {
-    
+
         element: HTMLElement;
-    
+
         constructor(options: {
             title?: string,
             transparent?: boolean,
@@ -132,29 +152,29 @@ declare module "siyuan"{
             disableClose?: boolean
             disableAnimation?: boolean
         });
-    
+
         destroy(options?: IObject): void;
-    
+
         bindInput(inputElement: HTMLInputElement | HTMLTextAreaElement, enterEvent?: () => void): void;
     }
-    
+
     export class Menu {
         constructor(id?: string, closeCB?: () => void);
-    
+
         showSubMenu(subMenuElement: HTMLElement): void;
-    
+
         addItem(options: IMenuItemOption): HTMLElement;
-    
+
         addSeparator(): void;
-    
+
         open(options: { x: number, y: number, h?: number, w?: number, isLeft?: boolean }): void;
-    
+
         /*
          * @param {string} [position=all]
          */
         fullscreen(position?: "bottom" | "all"): void;
-    
+
         close(): void;
     }
-    
+
 }
