758951eae892b22f5957a3f5a1e608f5836f02e4
frostime <frostime@foxmail.com>
2023-05-12 18:28:24 +0800

    feat: 插件初始化 + setting 导入导出

--- src/global-setting.ts
+++ src/global-setting.ts
@@ -7,10 +7,11 @@ import { info, error } from './utils';
 
 type NotebookSorting = 'doc-tree' | 'custom-sort'
 type NotebookView = 'Selector' | 'Menu'
+type SettingKey = 'OpenOnStart' | 'NotebookSort' | 'NotebookView'
 
 class SettingManager {
     plugin: Plugin;
-    settings = {
+    settings: any = {
         OpenOnStart: true as boolean,
         NotebookSort: 'custom-sort' as NotebookSorting,
         NotebookView: 'Selector' as NotebookView
@@ -20,13 +21,14 @@ class SettingManager {
         this.plugin = plugin;
     }
 
-    get(key: string) {
+    get(key: SettingKey) {
         return this.settings?.[key];
     }
 
-    set(key: string, value: any) {
+    set(key: any, value: any) {
+        info(`Setting update: ${key} = ${value}`)
         if (!(key in this.settings)) {
-            error(`Setting ${key} not found`);
+            error(`"${key}" is not a setting`);
             return;
         }
 
@@ -37,19 +39,23 @@ class SettingManager {
      * 导入的时候，需要先加载设置；如果没有设置，则使用默认设置
      */
     async load() {
-        let loaded = await this?.plugin.loadStorage('DailyNoteToday.json');
-        info(`Read storage: ${loaded}`)
-        if (loaded == null) {
+        let loaded = await this?.plugin.loadData('DailyNoteToday.json');
+        info(`Read storage:`)
+        console.log(loaded);
+        if (loaded == null || loaded == undefined || loaded == '') {
             //如果没有配置文件，则使用默认配置，并保存
+            info(`Setting not found, use default setting`)
             this.save();
         } else {
             //如果有配置文件，则使用配置文件
-            loaded = JSON.parse(loaded);
-            for (let key in this.settings) {
-                if (key in loaded) {
-                    info(`Setting ${key} = ${loaded[key]}`)
-                    this.settings[key] = loaded[key];
+            try {
+                for (let key in loaded) {
+                    this.set(key, loaded[key]);
                 }
+            } catch (error_msg) {
+                error(`Setting load error: ${error_msg}`);
+                console.log(error_msg);
+                this.save();
             }
         }
     }
@@ -57,7 +63,7 @@ class SettingManager {
     save() {
         let json = JSON.stringify(this.settings);
         info(`Write storage: ${json}`);
-        this.plugin.writeStorage('DailyNoteToday.json', json);
+        this.plugin.saveData('DailyNoteToday.json', json);
     }
 }
 
--- src/index.ts
+++ src/index.ts
@@ -1,104 +1,112 @@
 /**
  * Copyright (c) 2023 frostime. All rights reserved.
  */
-import { Plugin, clientApi } from 'siyuan';
-import Setting from './components/setting.svelte'
+import { Plugin } from 'siyuan';
+// import Setting from './components/setting.svelte'
 import { ToolbarMenuItem } from './components/toolbar-menu';
-import { ToolbarSelectItem } from './components/toolbar-select';
-import { ToolbarItem } from './components/interface';
-import { queryNotebooks, getDocsByHpath, openDiary, notify } from './func';
+import { notify } from './func';
 import { info, StaticText } from './utils';
 import { settings } from './global-setting';
 import notebooks from './global-notebooks';
 import { ContextMenu } from './components/move-menu';
 
 
-export default class SiyuanSamplePlugin extends Plugin {
+export default class DailyNoteTodayPlugin extends Plugin {
 
-    toolbar_item: ToolbarItem;
+    toolbar_item: ToolbarMenuItem;
 
     div_setting: HTMLElement;
-    component_setting: Setting;
+    //TODO 后续添加 setting 面板
+    // component_setting: Setting;
 
     menu: ContextMenu;
 
-    constructor() {
-        super();
-        info(`Start: ${new Date()}`);
-        settings.setPlugin(this);
+    test() {
+
     }
 
     async onload() {
-        let start = performance.now();
-        await notebooks.init();
+        info('plugin load');
+        console.log(this);
+        info(`Start: ${new Date()}`);
+        settings.setPlugin(this);
 
-        this.registerCommand({
-            command: 'updateAll',
-            shortcut: 'ctrl+alt+u,command+option+u',
-            description: '全局更新',
-            callback: this.updateAll.bind(this),
-        });
+        let start = performance.now();
+        // await notebooks.init();
+
+        //TODO 注册快捷键
+        // this.registerCommand({
+        //     command: 'updateAll',
+        //     shortcut: 'ctrl+alt+u,command+option+u',
+        //     description: '全局更新',
+        //     callback: this.updateAll.bind(this),
+        // });
 
         await settings.load();
         this.initSetting();
-        this.initMenu();
+        this.initContextMenu();
         this.initToolbarItem();
 
         // 如果有笔记本，且设置中允许启动时打开，则打开第一个笔记本
-        this.toolbar_item.autoOpenDailyNote();
-        this.toolbar_item.updateDailyNoteStatus();
+        // this.toolbar_item.autoOpenDailyNote();
+        // this.toolbar_item.updateDailyNoteStatus();
 
         let end = performance.now();
         info(`Onload, 耗时: ${end - start} ms`);
     }
 
-    initSetting() {
-        this.div_setting = document.createElement('div');
-        this.component_setting = new Setting({
-            target: this.div_setting,
-            props: {
-                contents: StaticText.Setting
-            }
-        });
+    private initSetting() {
+        //TODO 注册设置面板
+        // this.div_setting = document.createElement('div');
+        // this.component_setting = new Setting({
+        //     target: this.div_setting,
+        //     props: {
+        //         contents: StaticText.Setting
+        //     }
+        // });
 
-        this.registerSettingRender((el) => {
-            el.appendChild(this.div_setting);
-        })
+        // this.registerSettingRender((el) => {
+        //     el.appendChild(this.div_setting);
+        // })
 
-        this.component_setting.$on("updateAll", () => { this.updateAll() })
+        // this.component_setting.$on("updateAll", () => { this.updateAll() })
     }
 
-    initMenu() {
-        this.menu = new ContextMenu();
-        this.menu.bindMenuOnCurrentTabs();
-        this.menu.addEditorTabObserver();
+    private initContextMenu() {
+        // this.menu = new ContextMenu();
+        // this.menu.bindMenuOnCurrentTabs();
+        // this.menu.addEditorTabObserver();
     }
 
-    initToolbarItem() {
-        if (settings.settings.NotebookView === 'Selector') {
-            this.toolbar_item = new ToolbarSelectItem();
-        } else {
-            this.toolbar_item = new ToolbarMenuItem();
-        }
-        this.toolbar_item.updateNotebookStatus();
-        clientApi.addToolbarRight(this.toolbar_item.ele);
+    private initToolbarItem() {
+        // this.toolbar_item = new ToolbarMenuItem();
+        // this.toolbar_item.updateNotebookStatus();
+        // // clientApi.addToolbarRight(this.toolbar_item.ele);
+        // this.addTopBar({
+        //     icon: 'iconCalendar',
+        //     title: '今日笔记',
+        //     position: 'right',
+        //     callback: () => {
+        //         this.toolbar_item.showMenu();
+        //     }
+        // })
     }
 
 
-    async updateAll() {
+    private async updateAll() {
         info('updateAll');
-        await notebooks.update(); // 更新笔记本状态
-        this.toolbar_item.updateNotebookStatus(); //更新下拉框中笔记本显示
-        this.toolbar_item.updateDailyNoteStatus(); // 更新下拉框中的日记存在状态
+        // await notebooks.update(); // 更新笔记本状态
+        // this.toolbar_item.updateNotebookStatus(); //更新下拉框中笔记本显示
+        // this.toolbar_item.updateDailyNoteStatus(); // 更新下拉框中的日记存在状态
 
-        this.menu.bindMenuOnCurrentTabs();
-        notify(StaticText.UpdateAll, 'info', 2500);
+        // this.menu.bindMenuOnCurrentTabs();
+        // notify(StaticText.UpdateAll, 'info', 2500);
     }
 
     onunload() {
         info('plugin unload')
-        this.toolbar_item.release();
-        this.menu.removeEditorTabObserver();
+        // this.toolbar_item.release();
+        // this.menu.removeEditorTabObserver();
         settings.save();
     }
 }
