f73b3228f9d0686e0cbe88a7a2488e51aff090a0
frostime <frostime@foxmail.com>
2023-05-17 15:09:18 +0800

    重写了 setting-item 组件，使其更容易复用 close #31

--- src/components/setting.svelte
+++ src/components/setting.svelte
@@ -1,7 +1,6 @@
 <script>
     import { createEventDispatcher, onDestroy } from "svelte";
     import { settings } from "../global-setting";
-    import { eventBus } from "../event-bus";
     import SettingItem from "./setting-item.svelte";
     let checked = settings.get("OpenOnStart");
     let notebookSort = settings.get("NotebookSort");
@@ -22,83 +21,34 @@
 </script>
 
 <div class="config__tab-container">
-    <SettingItem content={contents.autoOpen}>
-        <input
-            class="b3-switch fn__flex-center"
-            id="openDNOnStart"
-            type="checkbox"
-            bind:checked
-            on:change={(e) => {
-                eventBus.publish(eventBus.EventSetting, {
-                    key: "OpenOnStart",
-                    value: e.target.checked,
-                });
-            }}
-        />
-    </SettingItem>
-    <SettingItem content={contents.defaultNotebook}>
-        <input
-            class="b3-text-field fn__flex-center fn__size200"
-            id="defaultNotebook"
-            placeholder="请复制笔记本的 ID"
-            bind:value={defaultNotebook}
-            on:change={(e) => {
-                eventBus.publish(eventBus.EventSetting, {
-                    key: "DefaultNotebook",
-                    value: defaultNotebook,
-                });
-            }}
-        />
-    </SettingItem>
-    <SettingItem content={contents.sorting}>
-        <select
-            class="b3-select fn__flex-center fn__size200"
-            id="notebookSort"
-            bind:value={notebookSort}
-            on:change={(e) => {
-                let value = e.target.value;
-                eventBus.publish(eventBus.EventSetting, {
-                    key: "NotebookSort",
-                    value: value,
-                });
-            }}
-        >
-            <option value="custom-sort"
-                >{contents.sorting.options["custom-sort"]}</option
-            >
-            <option value="doc-tree"
-                >{contents.sorting.options["doc-tree"]}</option
-            >
-        </select>
-    </SettingItem>
-    <SettingItem content={contents.position}>
-        <select
-            class="b3-select fn__flex-center fn__size200"
-            id="iconPosition"
-            bind:value={iconPosition}
-            on:change={(e) => {
-                let value = e.target.value;
-                eventBus.publish(eventBus.EventSetting, {
-                    key: "IconPosition",
-                    value: value,
-                });
-            }}
-        >
-            <option value="left"
-                >{contents.position.options["left"]}</option
-            >
-            <option value="right"
-                >{contents.position.options["right"]}</option
-            >
-        </select>
-    </SettingItem>
-    <SettingItem content={contents.update}>
-        <button
-            class="b3-button b3-button--outline fn__flex-center fn__size200"
-            id="updateNotebookStatus"
-            on:click={onClick}
-        >
-            {contents.update.button}
-        </button>
-    </SettingItem>
+    <SettingItem
+        type="checkbox"
+        content={contents.autoOpen}
+        settingKey="OpenOnStart"
+        settingValue={checked}
+    />
+    <SettingItem
+        type="input"
+        content={contents.defaultNotebook}
+        settingKey="DefaultNotebook"
+        settingValue={defaultNotebook}
+    />
+    <SettingItem
+        type="select"
+        content={contents.sorting}
+        settingKey="NotebookSort"
+        settingValue={notebookSort}
+    />
+    <SettingItem
+        type="select"
+        content={contents.position}
+        settingKey="IconPosition"
+        settingValue={iconPosition}
+    />
+    <SettingItem
+        type="button"
+        content={contents.update}
+        settingKey="Update"
+        on:click={onClick}
+    />
 </div>
