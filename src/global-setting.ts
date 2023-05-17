5ea1fcc4c8f7d9b122772991bb23bccc27424dce
frostime <frostime@foxmail.com>
2023-05-18 00:37:07 +0800

    我又改了回来

--- src/global-setting.ts
+++ src/global-setting.ts
@@ -21,7 +21,7 @@ class SettingManager {
     plugin: Plugin;
     settings: any = {
         OpenOnStart: true as boolean, //启动的时候自动打开日记
-        DiaryUpToDate: true as boolean, //自动更新日记的日期
+        DiaryUpToDate: false as boolean, //自动更新日记的日期
         NotebookSort: 'custom-sort' as NotebookSorting, //笔记本排序方式
         DefaultNotebook: '', //默认笔记本的 ID
         IconPosition: 'left' as IconPosition //图标放置位置
