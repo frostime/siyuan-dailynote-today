4225d7b58da21ffcc4fa767849de33ab26f02fb5
frostime <frostime@foxmail.com>
2023-05-22 22:00:14 +0800

    msg

--- src/index.ts
+++ src/index.ts
@@ -134,7 +134,7 @@ export default class DailyNoteTodayPlugin extends Plugin {
 
             if (version !== settings.get('PluginVersion')) {
                 settings.set('PluginVersion', version);
-                notify(`${this.i18n.Name} ${this.i18n.NewVer}: v${version}`, 'info', 1500);
+                notify(`${this.i18n.Name}${this.i18n.NewVer}: v${version}`, 'info', 1500);
                 settings.save();
             }
         } catch (error_msg) {
