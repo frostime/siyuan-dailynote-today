bdb7ed20e6abdf060e46847b12ec2f35a648b843
frostime <frostime@foxmail.com>
2023-05-25 22:21:36 +0800

    TypeCheck for i18n

--- src/utils.ts
+++ src/utils.ts
@@ -1,6 +1,21 @@
-/**
- * Copyright (c) 2023 frostime. All rights reserved.
- */
+import zh_Hans from "./i18n/zh_CN.json";
+import en from "./i18n/en_US.json";
+
+export type I18N = typeof zh_Hans;
+
+type IsEqual<T, U> = 
+    (<T1>() => T1 extends T ? 1 : 2) extends
+    (<T2>() => T2 extends U ? 1 : 2)
+    ? true : false;
+
+
+export declare function i18NCheck<T, U>(): IsEqual<T, U>;
+export declare function i18NChecks(checks: true[]): void;
+
+i18NChecks([
+    i18NCheck<I18N, typeof zh_Hans>(),
+    i18NCheck<I18N, typeof en>()
+]);
 
 export function info(...msg: any[]): void {
     console.log(`[DailyNoteToday][INFO] ${msg}`);
@@ -14,7 +29,7 @@ export function warn(...msg: any[]): void {
     console.warn(`[DailyNoteToday][WARN] ${msg}`);
 }
 
-export let i18n: any;
+export let i18n: I18N;
 export function setI18n(i18n_: any) {
     i18n = i18n_;
 }
