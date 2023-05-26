ae84616b6c77563ff945ffedbbb7fba5f80e3ec9
frostime <frostime@foxmail.com>
2023-05-26 15:06:44 +0800

    i18n check

--- src/utils.ts
+++ src/utils.ts
@@ -1,6 +1,25 @@
 import zh_Hans from "./i18n/zh_CN.json";
+import en_US from "./i18n/en_US.json";
+import { showMessage } from "siyuan";
 
 export type I18N = typeof zh_Hans;
+
+export function checkI18n(): boolean {
+    function checkKeys(obj1: any, obj2: any): boolean {
+        if (typeof obj1 !== typeof obj2) return false;
+        if (typeof obj1 === "object") {
+            for (const key in obj1) {
+                if (!checkKeys(obj1[key], obj2[key])) return false;
+            }
+        }
+        return true;
+    }
+    return checkKeys(zh_Hans, en_US);
+}
+
+// if (!checkI18n()) {
+//     showMessage('i18n check failed', 5000, 'error');
+// }
 
 export function info(...msg: any[]): void {
     console.log(`[DailyNoteToday][INFO] ${msg}`);
