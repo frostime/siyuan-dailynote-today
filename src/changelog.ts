117cfa0be4f132d6c6b3d1249c177ed3fff48bd9
frostime <frostime@foxmail.com>
2023-05-27 14:21:56 +0800

    更改 version, fix changelog

--- src/changelog.ts
+++ src/changelog.ts
@@ -17,16 +17,19 @@ export async function showChangeLog(version: string) {
     try {
         const path = `/data/plugins/siyuan-dailynote-today/i18n/${i18n.ChangeLog.file}-${version}.md`;
         // const path = `/data/plugins/siyuan-dailynote-today/i18n/CHANGELOG_zh_CN-1.0.6.md`;
-        let file = await serverApi.getFile(path);
-        if (file !== null) {
-            //TODO 检查还能不能正常运行
-            let content = lute.Md2HTML(file);
-            showTypoDialog(
-                `${i18n.Name} v${version}`,
-                content,
-                isMobile() ? "92vw" : "60%"
-            );
+        let file: string = await serverApi.getFile(path);
+        let code404 = file.match(/"code":404/g);
+        if (code404 !== null) {
+            console.log(`找不到更新文件：${path}`);
+            return;
         }
+
+        let content = lute.Md2HTML(file);
+        showTypoDialog(
+            `${i18n.Name} v${version}`,
+            content,
+            isMobile() ? "92vw" : "60%"
+        );
     } catch (err) {
         
     }
