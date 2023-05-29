f619c3128549698863d9a79da86ad2f17f1c23f3
frostime <frostime@foxmail.com>
2023-05-29 22:33:37 +0800

    copy to plugins

--- scripts/replace_plugin.js
+++ scripts/replace_plugin.js
@@ -1,4 +1,5 @@
 import fs from 'fs';
+import path from 'path';
 
 
 //************************************ Write you dir here ************************************
@@ -14,7 +15,8 @@ const targetDir = 'H:\\Media\\SiYuan\\data\\plugins';
 
 
 //Check
-if (!fs.existsSync(targetDir)) {
+if (!fs.existsSync(targetDir))
+{
     console.log(`Failed! plugin directory not exists: "${targetDir}"`);
     console.log(`Please set the plugin directory in scripts/make_dev_link.js`);
     process.exit(1);
@@ -22,7 +24,8 @@ if (!fs.existsSync(targetDir)) {
 
 
 //check if plugin.json exists
-if (!fs.existsSync('./plugin.json')) {
+if (!fs.existsSync('./plugin.json'))
+{
     console.error('Failed! plugin.json not found');
     process.exit(1);
 }
@@ -30,24 +33,55 @@ if (!fs.existsSync('./plugin.json')) {
 //load plugin.json
 const plugin = JSON.parse(fs.readFileSync('./plugin.json', 'utf8'));
 const name = plugin?.name;
-if (!name || name === '') {
+if (!name || name === '')
+{
     console.log('Failed! Please set plugin name in plugin.json');
     process.exit(1);
 }
 
 const distDir = `./dist`;
 //mkdir if not exists
-if (!fs.existsSync(distDir)) {
+if (!fs.existsSync(distDir))
+{
     fs.mkdirSync(distDir);
 }
 
 const targetPath = `${targetDir}/${name}`;
-//如果已经存在，就删除
-if (fs.existsSync(targetPath)) {
+//如果已经存在，就重建目录
+if (fs.existsSync(targetPath))
+{
     console.log('目标文件已经存在, deleting...');
     fs.rmdirSync(targetPath, { recursive: true });
+    fs.mkdirSync(targetPath);
 }
 
-//创建软链接
-fs.symlinkSync(`${process.cwd()}/dist`, targetPath, 'junction');
-console.log(`Done! Created symlink ${targetPath}`);
+const srcPath = `${process.cwd()}/dist`;
+
+function copyFolderRecursiveSync(source, target)
+{
+    // 如果目标文件夹不存在则创建
+    if (!fs.existsSync(target))
+    {
+        fs.mkdirSync(target);
+    }
+
+    // 读取源文件夹中的所有文件和子文件夹
+    fs.readdirSync(source).forEach((file) =>
+    {
+        const sourceFilePath = path.join(source, file);
+        const targetFilePath = path.join(target, file);
+
+        // 如果是文件夹则递归拷贝
+        if (fs.lstatSync(sourceFilePath).isDirectory())
+        {
+            copyFolderRecursiveSync(sourceFilePath, targetFilePath);
+        } else
+        {
+            // 否则直接拷贝文件到目标文件夹
+            fs.copyFileSync(sourceFilePath, targetFilePath);
+        }
+    });
+}
+
+copyFolderRecursiveSync(srcPath, targetPath);
+console.log(`Done! Copied to ${targetPath}`);
