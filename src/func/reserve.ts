7e419b66370b53f4f681270b8cfb1bd7eba823fd
frostime <frostime@foxmail.com>
2023-06-10 21:56:51 +0800

    refactor: 进一步提高 reserve.ts 的代码结构设计

--- src/func/reserve.ts
+++ src/func/reserve.ts
@@ -6,6 +6,14 @@ type RetvBlockContent = markdown;
 type RetvBlock = Block;
 type ResvBlockIds = BlockId[];
 
+function clipString(str: string, len: number) {
+    if (str.length > len) {
+        return str.slice(0, len) + '...';
+    } else {
+        return str;
+    }
+}
+
 /**
  * Retrieve 代指插入到日记中的预约的记录
  * 可以是嵌入块，链接或者双链等
@@ -56,8 +64,44 @@ export abstract class Retrieve {
         serverApi.setBlockAttrs(this.retvBlock.id, { name: 'Reservation', breadcrumb: "true" });
     }
 
-    abstract insert(): void;
-    abstract update(): void;
+    /**
+     * 查询预约块的内容
+     * @returns interfact {id: BlockId, content: string|undefined}
+     */
+    async retrieveResvBlocks() {
+        let retrieveRes = [];
+        for (let id of this.resvBlockIds) {
+            let block: Block = await serverApi.getBlockByID(id);
+            console.log(id, '-->', block);
+            if (block) {
+                retrieveRes.push({
+                    id: block.id,
+                    content: clipString(block.content, 20),
+                });
+            } else {
+                retrieveRes.push({
+                    id: id,
+                    content: undefined,
+                });
+            }
+        }
+        return retrieveRes;
+    }
+
+    async insert() {
+        let content = await this.createContent();
+        this.insertRetrieve(content);
+    }
+
+    async update() {
+        let content = await this.createContent();
+        this.updateRetrieve(content);
+    }
+
+    /**
+     * 子类实现，创建插入块的内容
+     */
+    abstract createContent(): RetvBlockContent|Promise<RetvBlockContent>;
 }
 
 
@@ -65,54 +109,17 @@ export abstract class Retrieve {
  * 将预约作为嵌入块插入到日记中
  */
 export class RetvAsEmbed extends Retrieve {
-
-    async insert() {
+    createContent() {
         let resvBlockIds = this.resvBlockIds.map((id) => `"${id}"`);
         let sql = `select * from blocks where id in (${resvBlockIds.join(',')})`;
         let sqlBlock = `{{${sql}}}`;
-        this.insertRetrieve(sqlBlock);
-    }
-
-    async update() {
-        let resvBlockIds = this.resvBlockIds.map((id) => `"${id}"`);
-        let sql = `select * from blocks where id in (${resvBlockIds.join(',')})`;
-        let sqlBlock = `{{${sql}}}`;
-        this.updateRetrieve(sqlBlock);
-    }
-}
-
-function clipString(str: string, len: number) {
-    if (str.length > len) {
-        return str.slice(0, len) + '...';
-    } else {
-        return str;
+        return sqlBlock;
     }
 }
 
 export class RetvAsLink extends Retrieve {
 
-    async retrieveResvBlocks() {
-        let retrieveRes = [];
-        for (let id of this.resvBlockIds) {
-            let block: Block = await serverApi.getBlockByID(id);
-            console.log(id, '-->', block);
-            if (block) {
-                retrieveRes.push({
-                    id: block.id,
-                    content: clipString(block.content, 20),
-                });
-            } else {
-                retrieveRes.push({
-                    id: id,
-                    content: undefined,
-                });
-            }
-        }
-        return retrieveRes;
-    }
-
-    async insert() {
-        // let resvBlockLinks = this.resvBlockIds.map((id) => `siyuan://blocks/${id}`);
+    async createContent() {
         let resvBlocks = await this.retrieveResvBlocks();
         let retrieveBlockList = [];
         for (let block of resvBlocks) {
@@ -123,70 +130,43 @@ export class RetvAsLink extends Retrieve {
             }
         }
         let retvBlockContent = retrieveBlockList.join('\n');
-        this.insertRetrieve(retvBlockContent);
-    }
-    async update() {
-        let resvBlocks = await this.retrieveResvBlocks();
-        let retrieveBlockList = [];
-        for (let block of resvBlocks) {
-            if (block.content) {
-                retrieveBlockList.push(`* [${block.content}](siyuan://blocks/${block.id})`);
-            } else {
-                retrieveBlockList.push(`* ${block.id} not found`);
-            }
-        }
-        let retvBlockContent = retrieveBlockList.join('\n');
-        this.updateRetrieve(retvBlockContent);
+        return retvBlockContent;
     }
 }
 
 export class RetvAsRef extends Retrieve {
 
-    async retrieveResvBlocks() {
-        let retrieveRes = [];
-        for (let id of this.resvBlockIds) {
-            let block: Block = await serverApi.getBlockByID(id);
-            console.log(id, '-->', block);
-            if (block) {
-                retrieveRes.push({
-                    id: block.id,
-                    content: clipString(block.content, 20),
-                });
+    async createContent() {
+        let resvBlocks = await this.retrieveResvBlocks();
+        let retrieveBlockList = [];
+        for (let block of resvBlocks) {
+            if (block.content) {
+                retrieveBlockList.push(`* ((${block.id} "${block.content}"))`);
             } else {
-                retrieveRes.push({
-                    id: id,
-                    content: undefined,
-                });
+                retrieveBlockList.push(`* ${block.id} not found`);
             }
         }
-        return retrieveRes;
+        let retvBlockContent = retrieveBlockList.join('\n');
+        return retvBlockContent;
     }
+}
 
-    async insert() {
-        // let resvBlockLinks = this.resvBlockIds.map((id) => `siyuan://blocks/${id}`);
-        let resvBlocks = await this.retrieveResvBlocks();
-        let retrieveBlockList = [];
-        for (let block of resvBlocks) {
-            if (block.content) {
-                retrieveBlockList.push(`* ((${block.id} "${block.content}"))`);
-            } else {
-                retrieveBlockList.push(`* ${block.id} not found`);
-            }
-        }
-        let retvBlockContent = retrieveBlockList.join('\n');
-        this.insertRetrieve(retvBlockContent);
+export type RetvType = 'embed' | 'link' | 'ref';
+
+export function RetvFactory(type: RetvType, position: RetvPosition, resvBlockIds: ResvBlockIds, docId: DocumentId) {
+    let retv: Retrieve;
+    switch (type) {
+        case 'embed':
+            retv = new RetvAsEmbed(position, resvBlockIds, docId);
+            break;
+        case 'link':
+            retv = new RetvAsLink(position, resvBlockIds, docId);
+            break;
+        case 'ref':
+            retv = new RetvAsRef(position, resvBlockIds, docId);
+            break;
+        default:
+            throw new Error('unknown type');
     }
-    async update() {
-        let resvBlocks = await this.retrieveResvBlocks();
-        let retrieveBlockList = [];
-        for (let block of resvBlocks) {
-            if (block.content) {
-                retrieveBlockList.push(`* ((${block.id} "${block.content}"))`);
-            } else {
-                retrieveBlockList.push(`* ${block.id} not found`);
-            }
-        }
-        let retvBlockContent = retrieveBlockList.join('\n');
-        this.updateRetrieve(retvBlockContent);
-    }
+    return retv;
 }
