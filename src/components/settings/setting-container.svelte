aa2339b330ca491e2e9e71ceab94532a458c3738
frostime <frostime@foxmail.com>
2023-06-28 17:07:47 +0800

    新建组件 setting-container

--- src/components/settings/setting-container.svelte
+++ src/components/settings/setting-container.svelte
@@ -1,8 +1,17 @@
 <script lang="ts">
+    import SettingItem from "./setting-item.svelte";
+
     export let dataname: string;
-    export let settingItems;
+    export let settingItems: ISettingItem[];
 </script>
 
 <div class="config__tab-container" data-name={dataname}>
-
+    {#each settingItems as item (item.key)}
+        <SettingItem
+            type={item.type}
+            content={item.content}
+            settingKey={item.key}
+            settingValue={item.value}
+        />
+    {/each}
 </div>
