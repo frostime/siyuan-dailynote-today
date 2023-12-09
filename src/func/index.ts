26de71de8817403feeeadea8ecc14554498867dd
frostime <frostime@foxmail.com>
2023-12-09 19:17:11 +0800

    :sparkles: feat: 在同步后启动日记

--- src/func/index.ts
+++ src/func/index.ts
@@ -23,18 +23,28 @@ export class StartupEventHandler {
     plugin: DailyNoteTodayPlugin;
     eventBus: EventBus;
 
-    //About sync
-    isSyncChecked = false;
-    hasCheckSyncFor: number = 0;
-
     onSyncEndBindThis = this.onSyncEnd.bind(this);
+
+    flag = {
+        hasOpened: false,
+
+        openOnStart: false,
+        autoOpenAfterSync: false,
+
+        isSyncChecked: false,
+        hasCheckSyncFor: 0
+    }
 
     constructor(plugin: DailyNoteTodayPlugin) {
         this.plugin = plugin;
         this.eventBus = plugin.eventBus;
 
-        // this.openOnStart = settings.get('OpenOnStart');
-        // this.autoOpenAfterSync = settings.get('AutoOpenAfterSync');
+        this.flag.openOnStart = settings.get('OpenOnStart');
+        this.flag.autoOpenAfterSync = settings.get('AutoOpenAfterSync');
+
+        this.checkDuplicateDiary_Debounce = debouncer.debounce(
+            this.checkDuplicateDiary.bind(this), 2000, 'CheckDuplicateDiary'
+        );  // 防抖, 避免频繁检查
     }
 
     bindSyncEvent() {
@@ -46,46 +56,64 @@ export class StartupEventHandler {
     }
 
     async onPluginLoad() {
-        // const SYNC_ENABLED = window.siyuan.config.sync.enabled;
-        //如果思源没有开启同步, 就直接创建
-        await autoOpenDailyNote();
-        this.checkDuplicateDiary();
-        this.checkDuplicateDiary_Debounce = debouncer.debounce(
-            this.checkDuplicateDiary.bind(this), 2000, 'CheckDuplicateDiary'
-        );  // 防抖, 避免频繁检查
 
-        this.bindSyncEvent(); //绑定同步时间，以方便同步完成后检查是否有重复的日记
-
+        const SYNC_ENABLED = window.siyuan.config.sync.enabled;
+        if (!SYNC_ENABLED) {
+            //Case 1: 如果思源没有开启同步, 就直接创建, 并无需绑定同步事件
+            await this.tryToOpen();
+        } else if (this.flag.autoOpenAfterSync === false) {
+            //Case 2: 如果思源开启了同步, 但是用户没有设置在同步后打开, 就直接创建
+            this.bindSyncEvent();
+            this.tryToOpen();
+        } else {
+            //Case 3: 如果思源开启了同步, 并且用户设置了在同步后打开, 就绑定同步事件
+            this.bindSyncEvent();
+        }
     }
 
-    onSyncEnd({ detail }) {
-        console.log('sync-end', detail);
-        console.info('onSyncEnd', detail);
-        if (!this.isSyncChecked) {
+    async onSyncEnd({ detail }) {
+        console.debug('sync-end', detail);
+
+        if (this.flag.hasOpened === false && this.flag.autoOpenAfterSync === true) {
+            this.tryToOpen();
+        }
+
+        if (!this.flag.isSyncChecked) {
             this.checkDuplicateDiary_Debounce();
         }
     }
 
 
     public resetSyncCheckStatus() {
-        this.isSyncChecked = false; //重置同步检查状态
-        this.hasCheckSyncFor = 0; //重置同步检查次数
+        this.flag.isSyncChecked = false; //重置同步检查状态
+        this.flag.hasCheckSyncFor = 0; //重置同步检查次数
+        this.flag.hasOpened = false; //重置是否已经打开
     }
 
     /********** Duplicate **********/
     private async checkDuplicateDiary() {
         let hasDuplicate = await checkDuplicateDiary();
         if (hasDuplicate) {
-            this.isSyncChecked = true;
+            this.flag.isSyncChecked = true;
             this.unbindSyncEvent();
         }
-        this.hasCheckSyncFor++;
+        this.flag.hasCheckSyncFor++;
         //多次检查后，如果还是没有同步，则认为没有必要再检查了
-        if (this.hasCheckSyncFor >= MAX_CHECK_SYNC_TIMES) {
-            this.isSyncChecked = true;
+        if (this.flag.hasCheckSyncFor >= MAX_CHECK_SYNC_TIMES) {
+            this.flag.isSyncChecked = true;
             console.debug('关闭自动检查同步文件');
             this.unbindSyncEvent();
         }
     }
     private checkDuplicateDiary_Debounce: typeof this.checkDuplicateDiary = null;
+
+    private async tryToOpen() {
+        if (this.flag.openOnStart === false) return;
+        await autoOpenDailyNote();
+        this.flag.hasOpened = true;
+        setTimeout(() => {
+            this.checkDuplicateDiary();
+        }, 1500);
+    }
+
 }
