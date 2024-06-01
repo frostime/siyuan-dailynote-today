0c73e9c905a840a657c7a7cb78f7902d79cba048
frostime <frostime@foxmail.com>
2024-06-01 16:15:29 +0800

    ⬆️ dep: update svelte to version 4

--- svelte.config.js
+++ svelte.config.js
@@ -1,7 +1,15 @@
 import { vitePreprocess } from "@sveltejs/vite-plugin-svelte"
 
+const NoWarns = new Set([
+    "a11y-click-events-have-key-events",
+    "a11y-no-static-element-interactions",
+    "a11y-no-noninteractive-element-interactions"
+]);
+
 export default {
-    // Consult https://svelte.dev/docs#compile-time-svelte-preprocess
-    // for more information about preprocessors
     preprocess: vitePreprocess(),
+    onwarn: (warning, handler) => {
+        if (NoWarns.has(warning.code)) return;
+        handler(warning);
+    }
 }
